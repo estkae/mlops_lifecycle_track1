e3caa9289db439236d66fe7f26210a8cc09398f7
amudhan <amudhansubbiah@gmail.com>
2022-05-07 12:59:15 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Great Ways To Implement MLOPs',
+    author='Amudhan Subbiah',
+    license='MIT',
+)
